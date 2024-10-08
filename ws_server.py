35f6b8256f5a5f17deb0a6c4856580fdd15d158d
DerinSozen <67414435+DerinSozen@users.noreply.github.com>
2024-10-08 14:58:12 -0500

    Ctype import fix

--- ws_server.py
+++ ws_server.py
@@ -4,11 +4,17 @@ import whisper
 import re
 import asyncio
 import websockets
+from ctypes import *
 from datetime import datetime
 
+
+# From alsa-lib Git 3fd4ab9be0db7c7430ebd258f2717a976381715d
+# $ grep -rn snd_lib_error_handler_t
+# include/error.h:59:typedef void (*snd_lib_error_handler_t)(const char *file, int line, const char *function, int err, const char *fmt, ...) /* __attribute__ ((format (printf, 5, 6))) */;
+# Define our error handler type
 ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
 def py_error_handler(filename, line, function, err, fmt):
-    print "Alsa Error message"
+    print("Alsa Error message")
 c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
 
 async def handler(websocket):
